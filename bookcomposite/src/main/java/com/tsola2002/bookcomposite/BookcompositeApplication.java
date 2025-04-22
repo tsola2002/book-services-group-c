288df6f0143ddca28bb441ec87b525fa38c0ab5d
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-04-22 09:42:16 +0100

    Initial Commit

---
+++ bookcomposite/src/main/java/com/tsola2002/bookcomposite/BookcompositeApplication.java
@@ -0,0 +1,13 @@
+package com.tsola2002.bookcomposite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcompositeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcompositeApplication.class, args);
+	}
+
+}
