851aef29075171e4186a81f124740add7758f012
Olatubosun Eyinimofe Rasheedat <eyinimofeolatubosun@gmail.com>
2025-05-30 21:43:33 +0100

    added the book composite

--- bookservice/src/main/java/com/example/mofe/bookservice/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mofe.bookservice.repository;
-
-import com.example.mofe.bookservice.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
